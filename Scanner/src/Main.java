81f0d13d05be72b742dabcbc65fca23d7fa5b594
AyikobuaDaniel <47473527+AyikobuaDaniel@users.noreply.github.com>
2019-02-15 12:38:30 +0300

    Add files via upload

---
+++ Scanner/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     Scanner reader=new Scanner(System.in);
+     
+     System.out.print("Enter a number");
+    
+     
+     int anothernum=reader.nextInt();
+	}
+
+}
